0df0bf286f4686446114d3ae5c2914071ff47049
Daniel Thompson <dthompson@llnw.com>
2012-08-20 14:19:11 -0700

    Adds additional ignores for bash history

--- source/10_history.sh
+++ source/10_history.sh
@@ -4,7 +4,7 @@ shopt -s histappend
 # Verify the history
 shopt -s histverify
 
-export HISTIGNORE="l:la:ls:pwd"
+export HISTIGNORE="l:la:ls:pwd:[#*]"
 export HISTFILESIZE=9999
 export HISTSIZE=9999
 export HISTTIMEFORMAT="[%F %T %z] $PWD "
