7c51b438270909ef4b8ea671710795905b259b10
Daniel Thompson <daniel@ionize.net>
2017-03-04 20:00:59 -0500

    Clean up how linking happens (and the script)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# instead of realpath
+# Instead of realpath
 cd $(dirname $0)
+
+# Set script_path to working directory
 script_path=$(pwd)
 
-for file in $script_path/link/*; do
-  ln -sf $file ~/.$(basename $file)
+# Link items
+find "$script_path/link" -maxdepth 1 | while read item; do
+  echo "Linking: $item"
+  ln -sf $item ~/.$(basename $item)
+done
+
+# Make a pass deleting stale links, if any
+for i in ~/.*; do
+    [ ! -h $i ] && continue
+
+    # We have a link: Is it stale? If so, delete it ...
+    if [ ! -f $i ] && [ ! -d $i ]; then
+        echo "Deleting stale link: $i"
+        rm $i
+    fi
 done
